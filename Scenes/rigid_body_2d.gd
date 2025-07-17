a0d49d91cbf674d993da53ceb3f738033838a680
YoungRichShiner <matmelene@gmail.com>
2025-07-17 16:52:54 +1000

    Stable

--- Scenes/rigid_body_2d.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody2D
-
-func _physics_process(_delta):
-	if Input.is_action_pressed("ui_up"): apply_force(Vector2(0, -500))
-	if Input.is_action_pressed("ui_left"): angular_velocity = -3
-	if Input.is_action_pressed("ui_right"): angular_velocity = 3
-	if Input.is_action_pressed("ui_accept"): linear_velocity = Vector2.ZERO
